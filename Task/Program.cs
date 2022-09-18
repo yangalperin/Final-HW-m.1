81303ebdd05f80a71601617a43037c04dde4a1c3
Yan <yangalperin@gmail.com>
2022-09-19 02:24:16 +0300

    Solution

---
+++ Task/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] words = { "Hello", "2", "world", ":-" };
+string[] newWords = new string[words.Length];
+int count = 0;
+for (int i = 0; i < words.Length; i++)
+{
+    if (words[i].Length <= 3)
+    {
+        newWords[count] = words[i];
+        count++;
+
+    }
+
+}
+PrintArray(newWords);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
